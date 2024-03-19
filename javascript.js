b282d0106b0f866c4ead2d45fd8d6023410ded41
Samuel <samuelrivba@gmail.com>
2024-03-18 21:26:21 -0500

    Add wanderlog 1 comments

--- javascript.js
+++ javascript.js
@@ -30,35 +30,33 @@ function readLine() {
  */
 
 function groupTransactions(transactions) {
+  // First we create an empty object to keep the counts
   const counts = {};
 
+  // For each transaction we create the key tx or add one if already created
   transactions.forEach((tx) => {
-    counts[tx] = (counts[tx] || 0) + 1;
+    counts[tx] = (counts[tx] || 0) + 1; // If counts has the key tx add 1, otherwise start in 0 and add 1
   });
-
+  // Here we have an object with tx as the keys and the counts as the values
+  // Ex. { prune: 2, banana: 1 }
+  // I transform the object counts into an array for the return and order it
   const countsArray = Object.entries(counts).map(
-    ([tx, count]) => `${tx} ${count}`
+    ([tx, count]) => `${tx} ${count}` // This array receives each object entry in a string so the array ends up like ["prune 2", "banana 1"]
   );
+  console.log(countsArray);
   countsArray.sort((a, b) => {
-    const countA = parseInt(a.split(" ")[1]);
+    // Now it's time to order the array
+    const countA = parseInt(a.split(" ")[1]); // We get the counts from the array items: "prune 2" gets the 2
     const countB = parseInt(b.split(" ")[1]);
     if (countA === countB) {
-      const itemA = a.split(" ")[0];
+      // First if the counts are the same
+      const itemA = a.split(" ")[0]; // We get the tx names, same as with the counts: "prune 2" gets prune
       const itemB = b.split(" ")[0];
-      return itemA.localeCompare(itemB);
+      return itemA.localeCompare(itemB); // We compare alphabetically with locale, 1 indicates that itemB comes before itemA
     }
-    return countB - countA;
+    return countB - countA; // We compare counts, positive numbers means countB comes before countA
   });
-  return countsArray;
-  // Write your code here
-
-  //
-  // WARNING: Please do not use GitHub Copilot, ChatGPT, or other AI assistants
-  //          when solving this problem!
-  //
-  // We use these tools in our coding too, but in our interviews, we also don't
-  // allow using these, and want to see how we do without them.
-  //
+  return countsArray; // Finally we return the sorted array
 }
 
 function main() {
